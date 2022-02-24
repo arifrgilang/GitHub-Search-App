8641d14f5fd49026e237ebb1f6c05c74fed8ac17
Arif R Gilang <arifrgilang@gmail.com>
2022-02-24 12:25:37 +0700

    Resolve .gitignore not working

---
+++ app/src/main/java/com/arifrgilang/githubsearchapp/GitHubSearchApp.kt
@@ -0,0 +1,43 @@
+/*
+ *
+ *   DANA.id
+ *   PT. Espay Debit Indonesia Koe.
+ *   Copyright (c) 2017-2022. All Rights Reserved.
+ *
+ */
+
+package com.arifrgilang.githubsearchapp
+
+import android.app.Application
+import com.arifrgilang.githubsearchapp.di.AppComponent
+import com.arifrgilang.githubsearchapp.di.DaggerAppComponent
+import com.arifrgilang.githubsearchapp.di.module.AppModule
+import timber.log.Timber
+
+/**
+ * @author Arif R Gilang P (arif.rhizky@dana.id)
+ * @version GitHubSearchApp, v 2.0 2/24/2022 11:15 AM by Arif R Gilang P
+ */
+class GitHubSearchApp: Application() {
+
+    lateinit var appComponent: AppComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        initializeInjector()
+        initializeTimber()
+    }
+
+    private fun initializeInjector() {
+        appComponent = DaggerAppComponent.builder()
+            .appModule(AppModule(this))
+            .build()
+    }
+
+    private fun initializeTimber() {
+        if(BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+}
---
+++ app/src/main/java/com/arifrgilang/githubsearchapp/di/AppComponent.kt
@@ -0,0 +1,24 @@
+/*
+ *
+ *   DANA.id
+ *   PT. Espay Debit Indonesia Koe.
+ *   Copyright (c) 2017-2022. All Rights Reserved.
+ *
+ */
+
+package com.arifrgilang.githubsearchapp.di
+
+import com.arifrgilang.githubsearchapp.di.module.AppModule
+import dagger.Component
+import javax.inject.Singleton
+
+/**
+ * @author Arif R Gilang P (arif.rhizky@dana.id)
+ * @version AppComponent, v 2.0 2/24/2022 11:29 AM by Arif R Gilang P
+ */
+
+@Singleton
+@Component(modules = [AppModule::class])
+interface AppComponent {
+
+}
